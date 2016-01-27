ef8229fb147865efe56063a4365bd65d8e44e967
Stuart Wong <cgs.wong@gmail.com>
2016-01-27 12:04:22 -0600

    chore: cleanup

--- scripts/common/minimize.sh
+++ scripts/common/minimize.sh
@@ -2,8 +2,16 @@
 
 set -ex
 
+if type apt-get >/dev/null 2>&1; then
+    apt-get clean
+fi
+
+if type yum >/dev/null 2>&1; then
+    yum clean all
+fi
+
 sudo dd if=/dev/zero of=/EMPTY bs=1M || :
-sudo rm /EMPTY
+sudo rm -rf /EMPTY
 
 # In CentOS 7, blkid returns duplicate devices
 swap_device_uuid=`sudo /sbin/blkid -t TYPE=swap -o value -s UUID | uniq`
@@ -16,3 +24,7 @@ fi
 sudo /sbin/swapoff "$swap_device"
 sudo dd if=/dev/zero of="$swap_device" bs=1M || :
 sudo /sbin/mkswap ${swap_device_label:+-L "$swap_device_label"} ${swap_device_uuid:+-U "$swap_device_uuid"} "$swap_device"
+
+# Block until the empty file has been removed, otherwise, Packer
+# will try to kill the box while the disk is still full and that's bad
+sync
--- scripts/common/sshd.sh
+++ scripts/common/sshd.sh
@@ -5,4 +5,5 @@ set -ex
 # UseDNS is mostly useless and disabling speeds up logins
 sudo tee -a /etc/ssh/sshd_config <<EOF
 UseDNS no
+GSSAPIAuthentication no
 EOF
